9599f2c3b334d419ef5542ed4f3e91fdf155e782
Tanner Hoke <hoke-t@users.noreply.github.com>
2016-07-06 15:54:19 -0500

    Initial commit

--- Stats/AppDelegate.swift
+++ Stats/AppDelegate.swift
@@ -11,16 +11,34 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    let statusItem: NSStatusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0)) { [unowned self] in
+            // do some task
+            while true {
+                let temp = self.getCPUTemperature()
+                dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+                    // update some UI
+                    self.statusItem.title = String(temp)
+                }
+                sleep(1)
+            }
+            
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
+    
+    func getCPUTemperature() -> Double {
+        SMCOpen()
+        let temperature = SMCGetTemperature(UnsafeMutablePointer<Int8>((SMC_KEY_CPU_TEMP as NSString).UTF8String))
+        SMCClose()
+        return temperature
+    }
 
 }
 
